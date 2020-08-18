e75a82dbb56b70b516117d78b549e320c41c4252
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2020-08-18 18:55:46 -0500

    Formulario cargar MRI

--- apps/ImagenMRI/urls.py
+++ apps/ImagenMRI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from apps.ImagenMRI import views
 
 #Incluyo todas la urls de la apliccacion, para que sean leidas en las urls globales de django
 urlpatterns = [
+	path('registrarImagenMRI/', views.ImagenMRICreate, name="registrar_ImagenMRI"),
 
 ]
--- apps/ImagenMRI/views.py
+++ apps/ImagenMRI/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from apps.Usuario.models import Administrador
+from apps.ImagenMRI.models import ImagenMRI
+from apps.ImagenMRI.forms import RegistroFormularioImagenMRI
 
 # Create your views here.
+def ImagenMRICreate(request):
+	administrador = Administrador.objects.get(id=request.user.id)
+	if request.method == 'POST':
+		form = RegistroFormularioImagenMRI(request.POST or None ,request.FILES or None)
+		if form.is_valid():
+			model = ImagenMRI
+			template_name = "ImagenMRI/registrarImagenMRI.html"
+			form_class = RegistroFormularioImagenMRI
+			form.save()
+			messages.success(request, 'Se ha registrado la imagen de resonacia magnética con éxito.')
+			#permisoDoctor = Permission.objects.get(codename='is_doctor')
+			#my_user=Doctor.objects.get(id=form.save().id)
+			#my_user.user_permissions.add(permisoDoctor)
+			return redirect('home_admin')
+		else:
+			messages.error(request, 'No se ha podido registar Doctor.')
+			return redirect('home_admin')
+	else:
+		form = RegistroFormularioImagenMRI()
+	return render(request, 'ImagenMRI/registrarImagenMRI.html', {
+		'form': form,
+		#'admin': administrador
+})
